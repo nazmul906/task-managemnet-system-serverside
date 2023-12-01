1dd613abc5be2274558c487acf3900557363c60a
nazmul906 <nazmulhb96@gmail.com>
2023-12-02 03:42:07 +0600

    addtask api is created to insert data in db

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const taskcollection = client.db("taskmangement").collection("alltask");
+    app.post("/addtask", async (req, res) => {
+      //   console.log(req.body);
+      const addedtask = req.body;
+      const result = await taskcollection.insertOne(addedtask);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -34,7 +42,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
